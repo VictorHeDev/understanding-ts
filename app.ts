12fa790f5e93d1919251e98113e84a7b4acac095
Woodstock <victorhe97@gmail.com>
2022-07-07 00:40:48 -0400

    add Function type

--- app.ts
+++ app.ts
@@ -7,3 +7,9 @@ function printResult(num: number): void {
 }
 
 printResult(add(5, 12));
+
+// Function types describe parameters and return types
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+console.log(combineValues(8, 8));
